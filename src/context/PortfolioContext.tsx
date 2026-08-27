c8e7aa131af70c8c26a58ff249e9008b04d4ebe8
memduh443 <memduh443@gmail.com>
2026-08-28 00:17:46 +0300

    Update PortfolioContext.tsx

--- src/context/PortfolioContext.tsx
+++ src/context/PortfolioContext.tsx
@@ -925,7 +925,7 @@ export const PortfolioProvider: React.FC<{ children: ReactNode }> = ({ children 
       await signInWithGoogle();
     } catch (err) {
       console.error('Login error:', err);
-    
+    }
   };
 
   const logout = async () => {
